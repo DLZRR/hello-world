2cf940e5115f07b76798f6e9a531b5eb9ca9b037
DLZRR <danker.roozemond@gmail.com>
2018-02-21 22:56:51 +0100

    Add files via upload

---
+++ Tablemaker.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+lijst = [21, 52, 53283, 5081, 282, 13, 5257, 5503, 2099, 5035, 5609, 40, 5280, 417, 5188, 5008, 104, 2639, 255, 2448, 10674, 60152, 64, 5567]
+b = np.zeros(shape=(24,62))
+
+
+for i in range(len(lijst)):
+    
+    name = "OUTPUT/" + str(lijst[i]) + "/parameter_outvalues_" + str(lijst[i]) + ".txt"
+    a = np.loadtxt(name, skiprows = 4)
+    
+    b[i][0] = i
+    b[i][1] = lijst[i]
+    
+    #First the minimum (error) "parameter_e":
+    for j in range(20):
+        b[i][j+2] = a[2][j]
+    
+    #The maximum error "parameter_ep":
+    for j in range(20):
+        b[i][j+22] = a[3][j]
+        
+    #The "actual" value "parameter":
+    for j in range(20):
+        b[i][j+42] = a[1][j]
+        
+np.savetxt("THETABLE.txt", b)
